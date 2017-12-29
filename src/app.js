8e293c7ff5924d5976ecef327eb3f79a3379383c
binio <tbiniecki@gmail.com>
2017-12-29 12:56:38 +0000

    add Player object and app controller

--- src/app.js
+++ src/app.js
@@ -5,16 +5,23 @@ var questionController = (function(){
         this.rightAnswer = rightAnswer;
     };
 
+    var Player = function(name,score,questions){
+        this.name = name;
+        this.score = score;
+        this.questions = questions;
+    };
+
     var repository = {
-        questions: []
+        questions: [],
+        players:[]
     };
 
     return {
-        addQuestion: function(question){
-            repository.questions.push(question);
+        addQuestion: function(q,answers, answer){
+            repository.questions.push(new Question(q,answers,answer));
         },
-        newQuestion: function(q,as,ra){
-            return new Question(q,as,ra);
+        addPlayer: function(name, score, qsts){
+            repository.players.push(new Player(name, score,qsts));
         },
         testing: function(){
             return repository;
@@ -22,16 +29,53 @@ var questionController = (function(){
     };
 })();
 
-var gameController =(function (questionCtrl) {
+var UIController = (function(){
+    var DOMStrings = {
+        'playerOne': '.user-1',
+        'addPlayerBtn': '.add-player'
+    };
+
+    var addPlayerToUI = function(name){
+        var html = '<h3>%playerName%</h3>';
+        var newHtml = html.replace('%playerName%', name);
+        document.querySelector(DOMStrings.playerOne).insertAdjacentHTML('beforeend',newHtml);
+    };
+
+    return {
+        getDOMStrings: function(){
+            return DOMStrings;
+        },
+        addPlayer: function(name, score, questions){
+            //1. add player to play
+            questionController.addPlayer(name,score,questions);
+            //2. display in UI
+            addPlayerToUI(name);
+
+        }
+    };
+})();
+
+var gameController =(function (questionCtrl, uiCtrl) {
+    var DOMStrings = uiCtrl.getDOMStrings();
+
+    var setupEventListeners = function(){
+        document.querySelector(DOMStrings.addPlayerBtn).addEventListener('click',function(){
+            console.log('player to add');
+        });
+    };
     return {
         init : function(){
             console.log('game started....');
+            setupEventListeners();
 
         },
-        addQuestion: function(question) {
-            questionController.addQuestion(question);
+        addQuestion: function(question, answers, rightAnswer) {
+            questionController.addQuestion(question,answers,rightAnswer);
+        },
+        addPlayer: function(name,score,questions){
+            UIController.addPlayer(name,score,questions);
         }
     };
 
-})(questionController);
+})(questionController, UIController);
 gameController.init();
